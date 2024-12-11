447e3aa4f580a1ca5f2205466766c1f283ff2b53
Hajialil-Shaik <2200032298cser1@gmail.com>
2024-12-11 18:56:57 +0530

    nothing

--- src/main/java/com/klef/jfsd/springboot/SdpApplication.java
+++ src/main/java/com/klef/jfsd/springboot/SdpApplication.java
@@ -8,7 +8,7 @@ public class SdpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SdpApplication.class, args);
-		System.out.println("SDP Project Running....");
+		System.out.println("SDP Project Running....!");
 	}
 
 }
